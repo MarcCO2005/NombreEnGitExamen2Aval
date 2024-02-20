62df4e43b59cf5b4077529b0c3d4b04c30151a94
Marc Cuenca Olmos <mcuencaolmos@gmail.com>
2024-02-20 09:04:52 +0100

    a

---
+++ atotal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Calcular consumo de la ciudad:"
+read $ciu
+con=0
+
+while read line
+        do
+                a=`echo $line | awk '{print $1}'`
+                if [ $a = $ciu ]
+                        then
+                        aux=`echo $line | awk '{print $4}'`
+                        $con=$(( con + aux ))
+                     else
+                     echo "Vuelve a introducir la ciudad"
+                         
+                fi
+                
+                
+done < consumos.txt
+
+echo $con
